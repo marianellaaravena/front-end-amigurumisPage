e245bd36d8400c06ddb470dfafab0357b1479946
marianellaaravena <116914643+marianellaaravena@users.noreply.github.com>
2026-07-16 00:22:05 -0300

    Update API URL for category service

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -5,8 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class CategoryService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:3000/categories';
-
+private apiUrl = 'https://back-end-amigurumipage-1.onrender.com/categories';
   // Trae solo las categorías que están activas para tus botones del catálogo
   getActiveCategories(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/active`);
